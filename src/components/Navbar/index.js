25a91fda5259126d7597ef9f1544a6ddb5a91084
DashNothing <stokic.toni@gmail.com>
2021-07-06 12:30:24 +0200

    Fixed logo link path

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -25,7 +25,7 @@ const Navbar = () => {
 
 	return (
 		<header css={headerStyle}>
-			<Link to={`${process.env.PUBLIC_URL}`}>
+			<Link to={`${process.env.PUBLIC_URL}/`}>
 				<h1 css={logoStyle}>NutritionApp</h1>
 			</Link>
 			{renderSearchbar()}
